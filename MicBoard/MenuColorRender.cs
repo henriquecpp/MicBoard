baa83cc0e105b847214037c8d74ac26df620d14d
Henrique Marino <henriquemarino48@gmail.com>
2020-02-25 23:04:14 -0300

    adicionada a classe que renderiza as customizacoes do menuItem

---
+++ MicBoard/MenuColorRender.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicBoard
+{
+    class MenuColorRender : ProfessionalColorTable
+    {
+        public override Color ToolStripDropDownBackground
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color ImageMarginGradientBegin
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color ImageMarginGradientMiddle
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color ImageMarginGradientEnd
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color MenuBorder
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color MenuItemBorder
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color MenuItemSelected
+        {
+            get
+            {
+                return Color.FromArgb(64, 64, 64);
+            }
+        }
+
+        public override Color MenuStripGradientBegin
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color MenuStripGradientEnd
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color MenuItemSelectedGradientBegin
+        {
+            get
+            {
+                return Color.FromArgb(64, 64, 64);
+            }
+        }
+
+        public override Color MenuItemSelectedGradientEnd
+        {
+            get
+            {
+                return Color.FromArgb(64, 64, 64);
+            }
+        }
+
+        public override Color MenuItemPressedGradientBegin
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+
+        public override Color MenuItemPressedGradientEnd
+        {
+            get
+            {
+                return Color.Black;
+            }
+        }
+    }
+}
