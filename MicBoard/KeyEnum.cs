518bc5d50f2edd6012f5bb28458d29b644eaafe1
Henrique Marino <henriquemarino48@gmail.com>
2020-03-08 23:43:53 -0300

    Enum customizado, baseado no Enum padrao do WinForms, servindo como base para traduzir as teclas de forma que fique mais familiar (e.g. ControlKey vira Ctrl para o usuario)

---
+++ MicBoard/KeyEnum.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace MicBoard
+{
+    [Flags]
+    public enum KeyEnum
+    {
+        BackSpace = 8,
+        Shift = 16,
+        Ctrl = 17,
+        Alt = 18,
+        CapsLock = 20,
+        Esc = 27,
+        Space = 32,
+        PageUp = 33,
+        PageDown = 34,
+        End = 35,
+        Home = 36,
+        Left = 37,
+        Up = 38,
+        Right = 39,
+        Down = 40,
+        Insert = 45,
+        Delete = 46,
+        NumLock = 144
+    }
+}
