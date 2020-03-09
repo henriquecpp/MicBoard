e2e6fdf06f7bbcee3ec0cbe886eb2aa035667f63
Henrique Marino <henriquemarino48@gmail.com>
2020-03-08 23:41:42 -0300

    Classe com finalizade de reproduzir audio atraves do dispotisivo de entrada

---
+++ MicBoard/AudioIn.cs
@@ -0,0 +1,37 @@
+﻿using NAudio.Wave;
+
+namespace MicBoard
+{
+    class AudioIn
+    {
+        private static WaveOut Mic = null;
+        private static AudioFileReader audioFile = null;
+        public static void Play(string path)
+        {            
+            audioFile = new AudioFileReader(path);
+
+            Stop();
+            Mic = new WaveOut();
+            Mic.DeviceNumber = 1;
+
+            Mic.DesiredLatency = 700;
+            Mic.NumberOfBuffers = 3;
+            Mic.Volume = 0.5f;
+
+            Mic.Init(audioFile);
+
+            Mic.Play();;
+        }
+
+        public static void Stop()
+        {
+            if (Mic != null)
+            {
+                Mic.Stop();
+                Mic.Dispose();
+                Mic = null;
+            }
+        }
+
+    }
+}
