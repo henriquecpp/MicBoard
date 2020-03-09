44d4fa58aa7603377d295b4e30b818a858ad0b20
Henrique Marino <henriquemarino48@gmail.com>
2020-03-08 23:41:06 -0300

    Classe usada para reproduzir o audio no dispositivo de saída

---
+++ MicBoard/AudioOut.cs
@@ -0,0 +1,32 @@
+﻿using NAudio.Wave;
+
+namespace MicBoard
+{
+    class AudioOut
+    {
+        
+        private static WasapiOut Speaker = null;
+        private static AudioFileReader audioFile = null;
+        public static void Play(string path)
+        {            
+            audioFile = new AudioFileReader(path);
+
+            Stop();
+            Speaker = new WasapiOut();
+
+            Speaker.Init(audioFile);            
+
+            Speaker.Play();
+        }
+
+        public static void Stop()
+        {
+            if (Speaker != null)
+            {
+                Speaker.Stop();
+                Speaker.Dispose();
+                Speaker = null;
+            }
+        }
+    }
+}
