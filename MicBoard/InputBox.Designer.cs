0c73a3c3afe14bb6aa241951a8b78e47efe822de
Henrique Marino <henriquemarino48@gmail.com>
2020-03-13 12:14:29 -0300

    InputBox alterado para uma classe derivada de um Form convencional

---
+++ MicBoard/InputBox.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MicBoard
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputField = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Confirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputField
+            // 
+            this.InputField.Location = new System.Drawing.Point(43, 34);
+            this.InputField.Name = "InputField";
+            this.InputField.ReadOnly = true;
+            this.InputField.Size = new System.Drawing.Size(400, 20);
+            this.InputField.TabIndex = 0;
+            this.InputField.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyCapture);
+            this.InputField.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyRelease);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(43, 71);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(87, 23);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.ClearAll);
+            // 
+            // Confirm
+            // 
+            this.Confirm.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Confirm.Location = new System.Drawing.Point(356, 71);
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Size = new System.Drawing.Size(87, 23);
+            this.Confirm.TabIndex = 2;
+            this.Confirm.Text = "Ok";
+            this.Confirm.UseVisualStyleBackColor = true;
+            // 
+            // InputBox2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 111);
+            this.Controls.Add(this.Confirm);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.InputField);
+            this.Name = "InputBox2";
+            this.Text = "Insira a(s) tecla(s) de atalho:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputField;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Confirm;
+    }
+}
--- MicBoard/InputBox.cs
+++ MicBoard/InputBox.cs
@@ -1,84 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MicBoard
 {
-    class InputBox
+    public partial class InputBox : Form
     {
-        static TextBox textBox;
-        static string dicKey = "";
-        static string dicValue = "";
-        static Dictionary<string, string> KeyValues = new Dictionary<string, string>();
-        static List<int> keysPressed = new List<int>();
-        public static Dictionary<string, string> ShowDialog(string title)
+        public Dictionary<string, string> KeyValues { get; set; }
+        List<int> keysPressed = new List<int>();
+        public InputBox()
         {
-            Form prompt = new Form()
-            {
-                Width = 500,
-                Height = 150,
-                FormBorderStyle = FormBorderStyle.FixedDialog,
-                Text = title,
-                StartPosition = FormStartPosition.CenterScreen
-            };
-            textBox = new TextBox() { Left = 50, Top = 30, Width = 400 };
-            textBox.KeyDown += new KeyEventHandler(KeyCapture);
-            textBox.KeyUp += new KeyEventHandler(KeyRelease);
-            textBox.ReadOnly = true;
-
-            Button clear = new Button() { Left = 50, Width = 100, Top = 70, Text = "Clear" };
-            clear.Click += (sender, e) => { textBox.Text = ""; ClearAll(); textBox.Focus();};
-
-            Button comfirm = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
-            comfirm.Click += (sender, e) => { prompt.Close(); };
-
-            prompt.MaximizeBox = false;
-            prompt.Controls.Add(textBox);
-            prompt.Controls.Add(comfirm);
-            prompt.Controls.Add(clear);
-            prompt.AcceptButton = comfirm;
-            prompt.FormClosed += (sender, e) => { ClearAll(); };
-            
-            return prompt.ShowDialog() == DialogResult.OK ? new Dictionary<string, string>() { { dicKey.ToString(), dicValue.ToString() } } : null;
+            InitializeComponent();
+            this.MaximizeBox = false;
         }
-
-
-        private static void KeyCapture(object sender, KeyEventArgs e)
+        private void KeyCapture(object sender, KeyEventArgs e)
         {
             int key = (int)(Keys)(e.KeyCode);
-            
+
             if (!keysPressed.Contains(key))
             {
-                
+
                 keysPressed.Add(key);
 
                 KeyValues = new HotKeyManager().Sort(string.Join("+", keysPressed.ToArray()));
-                dicKey = KeyValues.Last().Key;
-                dicValue = KeyValues.Last().Value;                
-                textBox.Text = new HotKeyManager().Translate(Array.ConvertAll( KeyValues.First().Key.Split('+'), int.Parse) );
                 
-            }            
+                InputField.Text = new HotKeyManager().Translate(Array.ConvertAll(KeyValues.First().Key.Split('+'), int.Parse));
+
+            }
 
         }
-        private static void KeyRelease(object sender, KeyEventArgs e)
+        private void KeyRelease(object sender, KeyEventArgs e)
         {
             int key = (int)(Keys)(e.KeyCode);
-            
+
             keysPressed.Remove(key);
-            
+
 
         }
 
-        private static void ClearAll()
+        private void ClearAll(object sender, EventArgs e)
         {
             KeyValues.Clear();
             keysPressed.Clear();
+            InputField.Text = "";
+            InputField.Focus();
         }
-        
+
     }
 }
