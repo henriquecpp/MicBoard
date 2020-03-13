7223cff649ed8a7358e3e82f0e82dcb5795b6efe
Henrique Marino <henriquemarino48@gmail.com>
2020-03-12 22:22:32 -0300

    Classe com os atributos para guardar as preferencias do usuario (somente o volume por enquanto)

---
+++ MicBoard/UserPrefModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicBoard
+{
+    class UserPrefModel
+    {
+        public int SpeakerVolume { get; set; }
+        public int MicVolume { get; set; }
+    }
+}
