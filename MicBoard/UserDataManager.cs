764f31fae9a0ddaf8c13dfbe72f13ab58977c7c7
Henrique Marino <henriquemarino48@gmail.com>
2020-03-12 22:23:06 -0300

    Classe para salvar as preferencias do usuario no XML (so volume por enquanto)

---
+++ MicBoard/UserDataManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MicBoard
+{
+    class UserDataManager
+    {
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MicBoardUserSettings.xml";
+        public void SavePreferences(UserPrefModel u)
+        {
+            if (File.Exists(docPath)) Update(u);
+            else
+            {
+                XDocument doc = new XDocument(new XElement("UserPreferences", new XElement("Volume", 
+                    new XElement("SpeakerVolume", u.SpeakerVolume), new XElement("MicVolume", u.MicVolume))));
+                doc.Save(docPath);
+            }
+        }
+
+        public void Update(UserPrefModel u)
+        {
+            XDocument doc = XDocument.Load(docPath);
+            //como os elementos não são uma sequencia, é preciso especificar qual tag deve-se alterar
+            doc.Element("UserPreferences").Element("Volume").Element("SpeakerVolume").Value = u.SpeakerVolume.ToString();
+            doc.Element("UserPreferences").Element("Volume").Element("MicVolume").Value = u.MicVolume.ToString();
+
+            doc.Save(docPath);
+        }
+
+        public UserPrefModel LoadVolumeSettings()
+        {
+            UserPrefModel u = new UserPrefModel();
+
+            if (!File.Exists(docPath)) return null;
+
+            foreach (XElement level in XElement.Load(docPath).Elements("Volume"))
+            {
+                u.SpeakerVolume = int.Parse(level.Element("SpeakerVolume").Value);
+                u.MicVolume = int.Parse(level.Element("MicVolume").Value);
+            }
+
+            return u;
+        }
+    }
+}
